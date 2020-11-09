e65f054a68f87770051e01e0cab3defd63233d7e
conorshort <cshort@tcd.ie>
2020-11-09 18:49:05 +0000

    set up app

---
+++ calendarproj/calendarapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    shared_with = models.ManyToManyField('self', blank=True)
+
+class Event(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    event_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    shared_with = models.ManyToManyField(
+        get_user_model(), related_name='shared_events', blank=True)
