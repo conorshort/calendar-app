15dab326286e208f6595583593c0f2bd2e73b6d7
conorshort <cshort@tcd.ie>
2020-12-14 23:35:52 +0000

    update migrations for RDS

--- calendarproj/calendarproj/settings.py
+++ calendarproj/calendarproj/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+
 from conf.config import Config
-print(Config.debug)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -78,10 +80,7 @@ WSGI_APPLICATION = 'calendarproj.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': Config.db_settings
 }
 
 
