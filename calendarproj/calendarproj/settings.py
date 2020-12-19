e7aee74015c0b5a4ddc1a4f284820b6eecf89834
conorshort <cshort@tcd.ie>
2020-12-19 19:37:07 +0000

    set up whitenoise for static files

--- calendarproj/calendarproj/settings.py
+++ calendarproj/calendarproj/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+# import secret config config
 from conf.config import Config
 
 
@@ -123,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = "static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
